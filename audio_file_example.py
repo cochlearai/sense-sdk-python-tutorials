b857305317a93353dbbd50e4caacf05bcf5218de
Thibault Bougerolles <thibault.bougerolles@gmail.com>
2022-03-11 11:03:31 +0900

    Update audio_file_example.py

--- audio_file_example.py
+++ audio_file_example.py
@@ -66,7 +66,7 @@ params.audio_format = sense.AF_FLOAT32
 # if <= 0. will use all the threads available on the machine
 params.num_threads = -1
 
-if sense.SenseInit("{your-project-key}", params) <= 0:
+if sense.SenseInit("{your-project-key}", params) < 0:
     exit(-1)
 
 file = File()
